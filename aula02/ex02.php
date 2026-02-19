4e9947bc94c2ff2bb8d4956b64a2db3a9beec3cd
Otavio <jusileufaleceu@gmail.com>
2026-02-19 08:46:34 -0300

    aula 2

---
+++ aula02/ex02.php
@@ -0,0 +1,13 @@
+<?php
+    //VERIFICAR CONTEÚDO E TIPOS DE UMA VARIÁVEL
+    $x = 10.5;
+    echo "O valor de x é: $x <br>";
+    echo "O valor de x é: ". $x;
+    echo "<hr>";
+
+    //COMANDAOS DE DEPURAÇÃO DE CÓDIGOS
+
+    var_dump($x);
+    print_r($x);
+
+?>
---
+++ aula02/ex05.php
@@ -0,0 +1,13 @@
+<?php
+    $dia = "segunda";
+    switch($dia){
+        case"segunda":
+                echo"Inicio da semana!";
+                break;
+        case"sexta":
+                echo"Fim de semana chegando!";
+                break;
+        default:
+                "Dia normal.";    
+        }
+?>
