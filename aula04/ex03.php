aafb9bfce50d3ca7325c0ea67c7d0f857c9c2fe9
Otavio <jusileufaleceu@gmail.com>
2026-03-12 08:19:25 -0300

    Add files via upload

---
+++ aula04/ex03.php
@@ -0,0 +1,27 @@
+<?php 
+    // 3. Faça um programa PHP que crie duas variáveis e atribua dois valores inteiros.
+    // Apresente todas as operações: adição, subtração, divisão, multiplicação e resto
+    // da divisão utilizando os operadores de atribuição.
+
+    $v1 = 10;
+    $v2 = 45;
+
+    $soma = $v1 + $v2;
+    echo "Soma: $soma ";
+    echo "<hr>";
+
+    $sub = $v1 - $v2;
+    echo "Subtração: $sub";
+    echo "<hr>";
+
+    $mult = $v1 * $v2;
+    echo "Multiplicação: $mult";
+    echo "<hr>";
+
+    $div = $v1 / $v2;
+    echo "Divisão: $div";
+    echo "<hr>";
+
+    $resto = $v1 % $v2;
+    echo "Resto da divisão: $resto";
+?>
