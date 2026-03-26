d3ff8150656ec6ba537e9fc0ccc59a7278c7738f
Otavio <jusileufaleceu@gmail.com>
2026-03-26 12:20:19 -0300

    Add files via upload

---
+++ aula06/formulario.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>FORMULARIO DE LOGIN</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="processa.php" method="POST">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="senha">
+            </div>
+            <input type="submit" class="btn btn-primary" value="Logar">
+            <!-- <button type="submit" class="btn btn-primary">Logar</button>     -->
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ aula06/processa.php
@@ -0,0 +1,19 @@
+<?php 
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // email: adm@email
+    // senha: 1234
+
+    if ($email == 'adm@email.com' && $senha == '1234') {
+        //echo "Vamos para a área restrita";
+        $nome = 'Otávio';
+
+        header('Location: restrita.php?nome='.$nome);
+    } else {
+        //echo "Vamos para o formulario";
+        header('Location: formulario.php');
+    }
+    
+
+?>
